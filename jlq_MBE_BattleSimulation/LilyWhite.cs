73daf3f2c532b2dac57ea81cb54d6ed3886645fb
Lead007 <2522060674@qq.com>
2016-05-12 16:05:04 +0800

    20160512
    刚才没推够100行，再推了两个技能

--- jlq_MBE_BattleSimulation/LilyWhite.cs
+++ jlq_MBE_BattleSimulation/LilyWhite.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace JLQ_MBE_BattleSimulation
 {
@@ -12,13 +13,38 @@ namespace JLQ_MBE_BattleSimulation
 		public LilyWhite(int id, Point position, Group group, Random random, Game game)
 			: base(id, position, group, random, game)
 		{
-
+		    enterButton[0] = (s, ev) =>
+		    {
+		        game.DefaultButtonAndLabels();
+		        if (IsWhite)
+		        {
+		            game.Characters.Where(c => Calculate.Distance(c, this) <= SC01Range && c.Group == this.Group)
+		                .Aggregate((Brush) Brushes.White, (cu, c) => c.LabelDisplay.Background = Brushes.LightBlue);
+		        }
+		        else
+		        {
+		            game.Characters.Where(c => IsInRangeAndEnemy(this.Position, SC01Range, c))
+		                .Aggregate((Brush) Brushes.White, (cu, c) => c.LabelDisplay.Background = Brushes.LightBlue);
+		        }
+		    };
+            SetDefaultLeaveSCButtonDelegate(0);
+		    enterPad[1] = (s, ev) =>
+		    {
+		        if (IsWhite && (!SC02WhiteIsLegalClick(game.MousePoint))) return;
+		        if ((!IsWhite) && (!IsEnemy(game[game.MousePoint]))) return;
+		        game.DefaultButtonAndLabels();
+		        game[game.MousePoint].LabelDisplay.Background = Brushes.LightBlue;
+		    };
+            SetDefaultLeavePadButtonDelegate(1);
 		}
 
         public bool IsWhite { get; set; } = true;
         private const int skillNum = 100;
         private const int SC01Range = 3;
         private const int SC01Num = 50;
+        private const float SC02Gain = -0.25f;
+        private const float SC02BlackGain = 1.5f;
+        private const float SC02BlackGain2 = 0.2f;
         private const int SC02Range = 2;
 
         public override void PreparingSection()
@@ -33,8 +59,19 @@ namespace JLQ_MBE_BattleSimulation
         {
             if (IsWhite)
             {
-                game.HandleIsTargetLegal = (SCee, point) => Calculate.Distance(SCee, this) <= SC01Range;
+                game.HandleIsTargetLegal = (SCee, point) =>
+                    Calculate.Distance(SCee, this) <= SC01Range && SCee.Group == this.Group && SCee != this;
                 game.HandleTarget = SCee => SCee.MpGain(SC01Num);
+            }
+            else
+            {
+                game.HandleIsTargetLegal = (SCee, point) => IsInRangeAndEnemy(this.Position, SC01Range, SCee);
+                game.HandleTarget = SCee =>
+                {
+                    HandleDoDanmakuAttack(SCee);
+                    var buff = new BuffGainDoDamage(SCee, this, this.Interval, SC02Gain, game);
+                    buff.BuffTrigger();
+                };
             }
         }
 
@@ -49,16 +86,29 @@ namespace JLQ_MBE_BattleSimulation
         {
             if (IsWhite)
             {
-                game.HandleIsLegalClick = SC02IsLegalClick;
+                game.HandleIsLegalClick = SC02WhiteIsLegalClick;
                 game.HandleIsTargetLegal = (SCee, point) => SCee.Position == point;
                 game.HandleTarget = SCee => SCee.MpGain(2*SCee.Attack);
             }
+            else
+            {
+                game.HandleIsLegalClick = point => IsEnemy(game[point]);
+                game.HandleIsTargetLegal = (SCee, point) => SCee.Position == point;
+                game.HandleTarget = SCee =>
+                {
+                    HandleDoDanmakuAttack(SCee, SC02BlackGain);
+                    var buff = new BuffGainDefence(SCee, this, 2*this.Interval, SC02BlackGain2, game);
+                    buff.BuffTrigger();
+                };
+            }
+            AddPadButtonEvent(1);
         }
 
         /// <summary>结束符卡02</summary>
         public override void EndSC02()
         {
             base.EndSC02();
+            RemovePadButtonEvent(1);
         }
         /// <summary>符卡03</summary>
         public override void SC03()
@@ -71,7 +121,17 @@ namespace JLQ_MBE_BattleSimulation
 
         }
 
-        private bool SC02IsLegalClick(Point point)
+        public override void SCShow()
+        {
+            AddSCButtonEvent(0);
+        }
+
+        public override void ResetSCShow()
+        {
+            RemoveSCButtonEvent(0);
+        }
+
+        private bool SC02WhiteIsLegalClick(Point point)
         {
             if (Calculate.Distance(point, this) > SC02Range) return false;
             var c = game[point];
